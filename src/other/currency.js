eb4e5f7be15f6954a11f6e6e58e47c50a0498f32
guzz <gsotero@gmail.com>
2017-10-04 18:26:55 -0300

    Update currency.js

--- src/other/currency.js
+++ src/other/currency.js
@@ -18,7 +18,7 @@ function currency (value, currency, decimals) {
     : stringified
   var i = _int.length % 3
   var head = i > 0
-    ? (_int.slice(0, i) + (_int.length > 3 ? ',' : ''))
+    ? (_int.slice(0, i) + (_int.length > 3 ? '.' : ''))
     : ''
   var _float = decimals
     ? stringified.slice(-1 - decimals)
@@ -26,6 +26,7 @@ function currency (value, currency, decimals) {
   var sign = value < 0 ? '-' : ''
   return sign + currency + head +
     _int.slice(i).replace(digitsRE, '$1,') +
+    '.' +
     _float
 }
 
